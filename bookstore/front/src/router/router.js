33d9028d7f5bb8ee223b1f7e8462817d9fbc37ea
miyoung-park <qkraldud1529@gmail.com>
2021-12-14 12:33:02 +0900

    setting book_store 211214

---
+++ bookstore/front/src/router/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Main from "@/views/Main";
+
+// 1. Vue - VueRouter 연결
+Vue.use(VueRouter);
+
+// 2. routes 정의
+const routes = [
+                    {
+                        path: '/',   // url 경로
+                        component : Main,// 매핑하고 싶은 컴포넌트(import필요!)
+                    }
+                ]
+
+// 3. router 인스턴스 생성
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
+// 4. export
+export default router;
---
+++ bookstore/front/src/views/Main.vue
@@ -0,0 +1,12 @@
+<template>
+    <h4> main </h4>
+</template>
+<script>
+export default {
+  name: "Main"
+}
+</script>
+
+<style scoped>
+
+</style>
